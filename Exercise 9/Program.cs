7456e9e16fd2abce4209fec6d5fe635b7a7d2e53
Joseph Furnari <joseph.furnari@s.capregboces.org>
2024-03-06 09:44:32 -0500

    Exercise 9 finished

--- Exercise 9/Program.cs
+++ Exercise 9/Program.cs
@@ -10,14 +10,26 @@
             Console.WriteLine("Enter number of rows: ");
             int length = Int32.Parse(Console.ReadLine());
 
-            for (int a = 0; a < width; a++)
+            for (int a = 1; a <= length; a++)
             {
-                for (int i = 0; i < length; i++)
+                for (int i = 1; i <= width; i++)
                 {
                     Console.Write("*");
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("Enter the MAX amount of asterisks: ");
+            int maxAsterisks = Int32.Parse(Console.ReadLine());
+
+            for (int a = 1; a <= maxAsterisks; a++)
+            {
+                for (int b = 1; b <= a; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
         }
     }
 }
